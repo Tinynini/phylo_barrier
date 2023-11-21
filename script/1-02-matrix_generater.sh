5b75e601730c5598e2670cd65c1404bb5e241f7b
Tinynini <66923814+Tinynini@users.noreply.github.com>
2023-11-21 16:31:47 +0100

    Update 1-02-matrix_generater.sh

--- script/1-02-matrix_generater.sh
+++ script/1-02-matrix_generater.sh
@@ -2,7 +2,7 @@
 
 # INPUT
 # species_ninon : listing des fichiers TSV associes aux especes traitees
-# cluster : listing des genes retenus comme representants parmis ceux a traiter apres clusterisation
+# cluster : output du clustering VSearch netoye (voir script 1-01)
 
 # OUTPUT
 # centro : 1ere colonne de la matrice (= label de genes)
@@ -16,29 +16,33 @@ centro='/home/ninon.robin/Methode_Generic/output/centro.tsv'
 species='/home/ninon.robin/Methode_Generic/output/species.tsv'
 share='/home/ninon.robin/Methode_Generic/output/share.tsv'
 
+# On recupere la liste des noms d'especes a partir de celle des chemins d'acces vers les fichiers TSV
 specie=$(cat ${species_ninon} | sed 's/^\/.*\/.*\/.*\//,/g' | sed 's/.tsv//g') 
 echo -n -e 'SPECIES'${specie}\\\n | sed 's/ //g' > ${species}
 
-centroid=$(cat ${cluster} | cut -f 10 | sort | uniq)
+centroid=$(cat ${cluster} | cut -f 10 | sort | uniq) # On recupere la liste des centroids (10eme champs)
 
-for centros in $(echo ${centroid})
+for centros in $(echo ${centroid}) # Pour chaque centroids
 do
-    echo ${centros}
+    # On enregistre le centroid dans une nouvelle ligne du fichier output prevu a cet effet
     echo ${centros} >> ${centro} # Ligne a commenter pour les fraction 2 a n si on travail sur des fractions !!
-    query=$(cat ${cluster} | grep ${centros} | cut -f 9 | sort | uniq)
-    query=$(echo ${query} | sed 's/ /|/g')
+    
+    query=$(cat ${cluster} | grep ${centros} | cut -f 9 | sort | uniq) # on preleve les genes associes au centroid
+    query=$(echo ${query} | sed 's/ /|/g') # on formate la liste de genes en vue d'un grep sur cette liste
 
-    for path in $(cat ${species_ninon})  
+    for path in $(cat ${species_ninon}) # Pour chaque especes bacteriennes
     do 
-        match=$(cut -f 1 ${path} | grep -E -c ${query})
-        if [ ${match} != 0 ]
+        # On cherche les genes selectionnes ci-avant dans le 1er champs du fichier TSV associe 
+        match=$(cut -f 1 ${path} | grep -E -c ${query}) # On ne recupere que le nombre de matchs
+        
+        if [ ${match} != 0 ] # Si le nombre de matchs est different de zero 
         then 
-            match=1
-            echo "${path} has a match"
+            match=1 # On lui donne la valeur 1 par defaut (binarisation de notre future matrice)
         fi
 
+         # On recupere le nombre de matchs dans une nouvelle colonne du fichier output prevu a cet effet
         echo -n ,${match} >> ${share}
     done 
 
-    printf \\\n >> ${share}
+    printf \\\n >> ${share} # On passe a la ligne suivante (un centroid == une ligne)
 done 
