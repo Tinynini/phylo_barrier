4f7b00896c5e3e91c81c3db0d28673c927eac412
Tinynini <66923814+Tinynini@users.noreply.github.com>
2023-11-21 16:32:11 +0100

    Update 1-03-matrix_solver.sh

--- script/1-03-matrix_solver.sh
+++ script/1-03-matrix_solver.sh
@@ -1,3 +1,5 @@
+# Methode d'assemblage de la matrice si les especes ont ete traitees en seul run du script precedent :
+
 centro='/home/ninon.robin/Methode_Generic/output/centro.tsv'
 species='/home/ninon.robin/Methode_Generic/output/species.tsv'
 share='/home/ninon.robin/Methode_Generic/output/share.tsv'
